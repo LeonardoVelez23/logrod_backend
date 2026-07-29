ee934eda0279052e011e985d317e3b9178f8a7bd
Samuel Vera <samuelveraloor16@gmail.com>
2026-07-29 14:22:17 -0500

    fix: productoRoutes

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { getAllProductos, getProductoById, createProducto, updateProducto, deleteProducto
-} from '../controllers/productoController.js';
+import { getAllProductos, getProductoById, createProducto, updateProducto, deleteProducto } from '../controllers/productoController.js'; import { verifyToken, restrictTo } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/', getAllProductos);
 router.get('/:id', getProductoById);
-router.post('/', createProducto);
-router.put('/:id', updateProducto);
-router.delete('/:id', deleteProducto);
+
+router.post('/', verifyToken, restrictTo('admin', 'empleado'), createProducto);
+router.put('/:id', verifyToken, restrictTo('admin', 'empleado'), updateProducto);
+router.delete('/:id', verifyToken, restrictTo('admin'), deleteProducto);
 
 export default router;
