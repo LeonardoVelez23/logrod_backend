3677304e6e23a49a61790d0d8a09fa589724bba0
Samuel Vera <samuelveraloor16@gmail.com>
2026-07-29 14:21:21 -0500

    fix: pedidoRoutes

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { getAllPedidos, getPedidoById, createPedido, updatePedido, deletePedido } from '../controllers/pedidoController.js';
+import { verifyToken, restrictTo } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
-router.get('/', getAllPedidos);
+router.use(verifyToken);
+
+router.get('/', restrictTo('admin', 'empleado'), getAllPedidos);
 router.get('/:id', getPedidoById);
-router.post('/', createPedido);
-router.put('/:id', updatePedido);
-router.delete('/:id', deletePedido);
+router.post('/', restrictTo('cliente', 'admin', 'empleado'), createPedido);
+router.put('/:id', restrictTo('admin', 'empleado'), updatePedido);
+router.delete('/:id', restrictTo('admin'), deletePedido);
 
 export default router;
