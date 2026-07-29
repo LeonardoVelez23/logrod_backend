4b253d4a014ed6ad078900881bfd7c8396e2535e
Samuel Vera <samuelveraloor16@gmail.com>
2026-07-28 20:20:06 -0500

    feat: implementar producto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import healthRoutes from './health.routes.js';
 import categoriaRoutes from './categoriaRoutes.js';
+import productoRoutes from './productoRoutes.js';
 
 const router = Router();
 
 router.use('/', healthRoutes);
 router.use('/categorias', categoriaRoutes);
+router.use('/productos', productoRoutes);
 
 export default router;
